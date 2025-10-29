d2a80afa72b75b237e905581a6e2dc65a36092fc
guidovarela <guido.varela@gmail.com>
2025-10-29 11:20:47 -0300

    subida de archivos - 29-10

---
+++ js/plugins.js
@@ -0,0 +1,7 @@
+// Fancybox
+// import Fancybox from "https://cdn.jsdelivr.net/npm/@fancyapps/ui@6.1/dist/fancybox/fancybox.umd.js"
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+
